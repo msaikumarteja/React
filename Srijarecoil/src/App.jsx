8d8c829eccfcc4cdb7d45e58b3d2f225c12c5f7b
msaikumarteja <saikumarteja.0309@gmail.com>
2024-06-08 18:09:28 +0530

    text update

--- Srijarecoil/src/App.jsx
+++ Srijarecoil/src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {CountAtom} from "./store/atom/count"
-import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil'
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { countAtom } from '../../../week-7-master/src/store/atoms/count'
 
 function App() {
   
@@ -17,6 +18,7 @@ function App() {
 }
 
 function Count(){
+  console.log("Hello")
   return(
     <div>
       <Number/>
@@ -26,7 +28,17 @@ function Count(){
 }
 
 function Number(){
+  // console.log("Hello 1")
   const count = useRecoilValue(CountAtom)
+  // if(count%2==0){
+  //   return(
+  //     <div>
+  //       {count}
+  //       <br />
+  //       it is even number
+  //     </div>
+  //   )
+  // }
   return(
     <div>
       {count}
@@ -34,14 +46,28 @@ function Number(){
   )
 }
 function Button(){
-  const [count,setCount] = useRecoilState(CountAtom)
+  const setCount = useSetRecoilState(CountAtom)
+
+  // const [count ,setCount] = useRecoilState(CountAtom)
 
   return(
     <div>
-      <button onClick={()=>{setCount(count+1)}}>Increment</button>
-      <button onClick={()=>{setCount(count-1)}}>Decrement</button>
+      <button onClick={()=>{setCount((count)=>count+1)}}>Increment</button>
+      <button onClick={()=>{setCount((count)=>count-1)}}>Decrement</button>
+      <Text/>
     </div>
   )
+}
+
+function Text(){
+  const value = useRecoilValue(countAtom)
+  if(value%2==0){
+    return(
+      <div>
+        it is even number
+      </div>
+    )
+  }
 }
 
 export default App
